a179cac87821001cdb6df0aeeb6160b02eb03d86
afarcilag <51205771+afarcilag@users.noreply.github.com>
2019-05-30 10:44:12 -0500

    Add files via upload

---
+++ 22052019_listas.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 10:06:36 2019
+
+@author: Andrés Felipe
+"""
+
+#pyton tutor
+
+#list (range(0,11))
+
+L3 = eval(input('Ingrese una lista: '))
+
+
+
+
+
+
+
